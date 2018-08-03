f128c8bd558e62eed725c06ab66b5cfcb5d4173d
Andrey Malyshev <imakensei@gmail.com>
2018-08-03 20:12:49 +0300

    Added list. Complete

--- app/src/main/kotlin/com/example/test/MainActivity.kt
+++ app/src/main/kotlin/com/example/test/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.test
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+import pojo.Contacts
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity(),
+                     ContactListFragment.OnContactListFragmentInteractionListener {
+
+    override fun onContactListFragmentInteraction(item: Contacts.Contact?) {
+        TODO("not implemented")
+    }
+
+    private val contactListFragment = ContactListFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+                              .add(R.id.contacts_container, contactListFragment)
+                              .commit()
     }
+
+
 }
